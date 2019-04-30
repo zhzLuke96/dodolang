4f9521bab5496531158da4409fe0f5d1bdfa17fd
sanshi <zhz961101@gmail.com>
2019-04-30 18:22:20 +0800

    test code

---
+++ src/machine/lexer_test.go
@@ -0,0 +1,89 @@
+package machine
+
+import (
+	"testing"
+)
+
+func TestLexer_Base(t *testing.T) {
+	token := "1"
+	needVar := "Number"
+	name, _ := GetTokenTypeName(token)
+	if name == needVar {
+		t.Log("Pass Lexer.base")
+	} else {
+		t.Errorf("Failed Lexer.base need %v but %v", needVar, name)
+	}
+
+	token = "'name'"
+	needVar = "String"
+	name, _ = GetTokenTypeName(token)
+	if name == needVar {
+		t.Log("Pass Lexer.base")
+	} else {
+		t.Errorf("Failed Lexer.base need %v but %v", needVar, name)
+	}
+
+	token = "mul"
+	needVar = "Operator"
+	name, _ = GetTokenTypeName(token)
+	if name == needVar {
+		t.Log("Pass Lexer.base")
+	} else {
+		t.Errorf("Failed Lexer.base need %v but %v", needVar, name)
+	}
+
+	token = "return"
+	needVar = "Instruction"
+	name, _ = GetTokenTypeName(token)
+	if name == needVar {
+		t.Log("Pass Lexer.base")
+	} else {
+		t.Errorf("Failed Lexer.base need %v but %v", needVar, name)
+	}
+}
+
+func TestLexer_InsWithArgs(t *testing.T) {
+	tokenHead := "dup"
+	tokenTail := "5"
+	token := tokenHead + "_" + tokenTail
+	needVar := "Instruction_Args"
+	name, arg := GetTokenTypeName(token)
+	if name == needVar && arg == tokenTail {
+		t.Log("Pass Lexer[Instruction_Args]")
+	} else {
+		t.Errorf("Failed Lexer[Instruction_Args] need %v,%v but %v,%v", needVar, tokenTail, name, arg)
+	}
+
+	tokenHead = "load"
+	tokenTail = "100086"
+	token = tokenHead + "_" + tokenTail
+	needVar = "Instruction_Args"
+	name, arg = GetTokenTypeName(token)
+	if name == needVar && arg == tokenTail {
+		t.Log("Pass Lexer[Instruction_Args]")
+	} else {
+		t.Errorf("Failed Lexer[Instruction_Args] need %v,%v but %v,%v", needVar, tokenTail, name, arg)
+	}
+}
+
+func TestLexer_Unknow(t *testing.T) {
+	token := "unknow_token"
+	needVar := "UNKNOW TOKEN"
+	name, _ := GetTokenTypeName(token)
+	if name == needVar {
+		t.Log("Pass Lexer.base")
+	} else {
+		t.Errorf("Failed Lexer.base need %v but %v", needVar, name)
+	}
+
+	// just support degitial args
+	tokenHead := "load"
+	tokenTail := "unknow_token"
+	token = tokenHead + "_" + tokenTail
+	name, _ = GetTokenTypeName(token)
+	if name == needVar {
+		t.Log("Pass Lexer[Instruction_Args]")
+	} else {
+		t.Errorf("Failed Lexer[Instruction_Args] need %v but %v", needVar, name)
+	}
+}
--- src/machine/machine_test.go
+++ src/machine/machine_test.go
@@ -1,1 +1,74 @@
 package machine
+
+import (
+	"testing"
+)
+
+const (
+	TestSymbol = "Machine_Test_Symbol"
+)
+
+func TestMachine_Run(t *testing.T) {
+	code := [...]string{"1", "num", "2", "num", "plus"}
+	m := NewMachine(code[:]).Run()
+
+	v, ok := m.Top(0).(int)
+	if ok && v == 3 {
+		t.Log("Pass Machine.Run")
+	} else {
+		t.Errorf("Failed Machine.Run need %v but %v", 3, v)
+	}
+}
+
+func TestMachine_Push(t *testing.T) {
+	code := [...]string{"1"}
+	m := NewMachine(code[:])
+	m.Push(TestSymbol)
+
+	v, ok := m.Top(0).(string)
+	if ok && v == TestSymbol {
+		t.Log("Pass Machine.Push")
+	} else {
+		t.Errorf("Failed Machine.Push need %v but %v", TestSymbol, v)
+	}
+}
+
+func TestMachine_Pop(t *testing.T) {
+	code := [...]string{"1"}
+	m := NewMachine(code[:])
+	m.Push(TestSymbol)
+
+	v, ok := m.Pop().(string)
+	if ok && v == TestSymbol {
+		t.Log("Pass Machine.Pop")
+	} else {
+		t.Errorf("Failed Machine.Pop need %v but %v", TestSymbol, v)
+	}
+}
+
+func TestMachine_Jump(t *testing.T) {
+	needVar := 1
+	code := [...]string{"1", "int", "'end'", "jump", "2", "int", "3", "int", "mul", "end:", "dup"}
+	m := NewMachine(code[:]).Run()
+
+	v, ok := m.Pop().(int)
+	if ok && v == needVar {
+		t.Log("Pass Machine.Jump")
+	} else {
+		t.Errorf("Failed Machine.Jump need %v but %v", needVar, v)
+	}
+}
+
+func TestMachine_Dup(t *testing.T) {
+	needVar := 1
+	code := [...]string{"1", "int", "dup"}
+	m := NewMachine(code[:]).Run()
+
+	m.Pop()
+	v, ok := m.Pop().(int)
+	if ok && v == needVar {
+		t.Log("Pass Machine.Dup")
+	} else {
+		t.Errorf("Failed Machine.Dup need %v but %v", needVar, v)
+	}
+}
